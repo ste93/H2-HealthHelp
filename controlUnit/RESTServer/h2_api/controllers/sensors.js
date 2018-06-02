30c6756b4a31e1ad6f20120f364ad0ce48e370af
Manuel Bottazzi <manuel.bottazzi@studio.unibo.it>
2018-06-02 15:06:21 +0200

    removing mongodb : fixed sensor elimination

--- controlUnit/RESTServer/h2_api/controllers/sensors.js
+++ controlUnit/RESTServer/h2_api/controllers/sensors.js
@@ -33,7 +33,7 @@ module.exports.createSensors = function (req, res) {
 
 module.exports.getSensorDetails = function (req, res) {
 
-  console.log("Get sensor details");
+  console.log("Get sensor " + req.params.sensorsID + "details");
 
   sensorFileManager.getSensor(req.params.sensorsID, function(error, jsonObj) {
     if (error) {
@@ -42,90 +42,17 @@ module.exports.getSensorDetails = function (req, res) {
       jsonUtilities.sendJsonResponse(res, 200, jsonObj);
     }
   });
-
-
-    /*sens
-      .find({'id' : req.params.sensorsID})
-      .exec(function(err, sensor) {
-        jsonUtilities.sendJsonResponse(res, 200, sensor);
-      });
-      */
 };
 
 
-/*module.exports.updateSensor = function (req, res) {
-  jsonUtilities.sendJsonResponse(res, 200, null);
-  /*if (! req.params.sensorsID) {
-    sendJsonResponse(res, 404, {
-      "message": "Not found, sensorID is required"
-    });
-    return;
-  }
-
-  var updatedSensorData = {
-    id : req.params.sensorsID,
-    name : req.formData.name,
-    patient : req.formData.patient,
-    system : req.formData.system,
-    value : req.formData.value,
-    unit : req.formData.unit
-  }
-
-  console.log("updatedData : " + JSON.stringify(updatedSensorData, null, 2));
-  */
-
-  /*sens
-    .find({'id' : req.params.sensorsID})
-    //.select(-id)
-    .exec(
-      function(err, sensor) {
-        if (!sensor) {
-          sendJsonResponse(res, 404, {
-            "message": "sensor not found"
-          });
-          return;
-        } else if (err) {
-          sendJsonResponse(res, 400, err);
-          return;
-        }
-        //sensor.id = req.params.sensorsID;
-        /*sensor.name = req.body.name;
-        sensor.patient = req.body.patient;
-        sensor.system = req.body.system;
-        sensor.value = req.body.value;
-        sensor.unit = req.body.unit;*/
-        /*var s = new sens(sensor)
-        s.save(function(err, s) {
-          if (err) {
-            sendJsonResponse(res, 404, err);
-          } else {
-            sendJsonResponse(res, 200, s);
-          }
-        });
-      }
-    );*/
-
 module.exports.deleteSensor = function (req, res) {
-  var sensorID = req.params.sensorID;
 
-  if (sensorID) {
-    sens
-      .deleteOne({id : sensorID})
-      .exec(
-        function(err, location) {
-          if (err) {
-            console.log("error: " + err);
-            jsonUtilities.sendJsonResponse(res, 404, err);
-            return;
-        }
-        jsonUtilities.sendJsonResponse(res, 204, null);
-        }
-      );
-    } else {
-      jsonUtilities.sendJsonResponse(res, 404, {
-        "message": "No sensorID found"
-      });
-    }
+  console.log("Deleting " + req.params.sensorsID + " sensor");
+
+  sensorFileManager.deleteSensor(req.params.sensorID);
+
+  jsonUtilities.sendJsonResponse(res, 204, null);
+
 };
 
 
