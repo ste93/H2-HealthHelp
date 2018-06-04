ea6b89c55b51e61719ed51bda2c93171cea84a32
Manuel Bottazzi <manuel.bottazzi@studio.unibo.it>
2018-06-04 17:50:55 +0200

    documentation and refactoring in API module

--- controlUnit/RESTServer/h2_api/controllers/sensors.js
+++ controlUnit/RESTServer/h2_api/controllers/sensors.js
@@ -1,6 +1,16 @@
+/**
+* Controller class to handle sensor-related operation.
+*/
+
 var jsonUtilities = require('../jsonUtilities');
 var sensorFileManager = require('../../control_unit_app/data_manager/saved_sensor_manager');
 
+/**
+ * Function that return the entire list of previuosly connected sensors (AKA configured).
+ * Read the list from file and then return to the requester in Json format.
+ * @param req Is the HTTP Request. Not used by this function.
+ * @param res Is the HTTP Response, returned to the requesting host with the result.
+ */
 module.exports.getSensorsList = function (req, res) {
 
   console.log("Get sensors list");
@@ -14,6 +24,31 @@ module.exports.getSensorsList = function (req, res) {
   });
 };
 
+/**
+ * Function that return the details of a specific sensor.
+ * Read the sensors' details from file and then return to the requester in Json format.
+ * @param req Is the HTTP Request. Holds the ID of the sensors to be searched.
+ * @param res Is the HTTP Response, returned to the requesting host with the result.
+ */
+module.exports.getSensorDetails = function (req, res) {
+
+  console.log("Get sensor " + req.params.sensorID + " details");
+
+  sensorFileManager.getSensor(req.params.sensorID, function(error, jsonObj) {
+    if (error) {
+      jsonUtilities.sendJsonResponse(res, 400, error);
+    } else {
+      jsonUtilities.sendJsonResponse(res, 200, jsonObj);
+    }
+  });
+};
+
+/**
+ * Function that add a sensor to the list and save his details.
+ * Read the various parameter of the sensor from request body and then save this to the file.
+ * @param req Is the HTTP Request. Holds in his body a json with all the informations of the sensor to be saved.
+ * @param res Is the HTTP Response, returned to the requesting host with the result code. The body can be null because no extra informations are returned.
+ */
 module.exports.createSensors = function (req, res) {
 
   console.log("New Sensor Creation : ");
@@ -27,40 +62,33 @@ module.exports.createSensors = function (req, res) {
   jsonUtilities.sendJsonResponse(res, 201, null);
 };
 
-module.exports.getSensorDetails = function (req, res) {
-
-  console.log("Get sensor " + req.params.sensorID + "details");
-
-  sensorFileManager.getSensor(req.params.sensorID, function(error, jsonObj) {
-    if (error) {
-      jsonUtilities.sendJsonResponse(res, 400, error);
-    } else {
-      jsonUtilities.sendJsonResponse(res, 200, jsonObj);
-    }
-  });
-};
-
-
+/**
+ * Function that remove a sensor from the list (if present).
+ * Read the ID of the sensor from request's parameters and then remove it from the file.
+ * @param req Is the HTTP Request. Holds the ID of the sensor to be removed.
+ * @param res Is the HTTP Response, returned to the requesting host with the result code. The body can be null because no extra informations are returned.
+ */
 module.exports.deleteSensor = function (req, res) {
 
   console.log("Deleting " + req.params.sensorID + " sensor");
 
   sensorFileManager.deleteSensor(req.params.sensorID);
-
   jsonUtilities.sendJsonResponse(res, 204, null);
-
 };
 
-
+/**
+ * Function to add data from a sensors.
+ * Read the ID of the sensor from request parameters and then ....
+ * @todo use data for something. ( save them in a list and send to the analyser).
+ * @param req Is the HTTP Request. Holds the ID of the sensors that measure the data.
+ * @param res Is the HTTP Response, returned to the requesting host with the result code. The body can be null because no extra informations are returned.
+ */
 module.exports.addData = function (req,res) {
   var sensorID = req.params.sensorID;
-  console.log("received data from " , sensorID);
+  console.log("Received data from " , req.params.sensorID);
   if(req.body){
     console.log("BODY: " , req.body);
   }
-
-
   //console.log("BODY: " + JSON.stringify(req.body, null, 2))
   jsonUtilities.sendJsonResponse(res, 200, null);
-
 };
